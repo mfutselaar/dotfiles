e5021affbd246fe91ee81c400e47953627d81f0c
Mathijs Futselaar <mathijs@matttt.nl>
2025-02-21 17:50:06 +0100

    k8s

--- resources/linux/k8s.sh
+++ resources/linux/k8s.sh
@@ -33,11 +33,15 @@ containerd config default | tee /etc/containerd/config.toml
 systemctl restart containerd
 systemctl enable containerd
 
-# Add Kubernetes repository and install Kubernetes
+# Add Kubernetes repository
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-bash -c 'cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
-deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.31/deb/ / main
-EOF'
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key | tee /etc/apt/keyrings/kubernetes-archive-keyring.gpg > /dev/null
+cat <<EOF > /etc/apt/sources.list.d/kubernetes.list
+deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.31/deb/ /
+EOF
+
+# Update package lists and install Kubernetes components
 apt-get update
 apt-get install -y kubelet kubeadm kubectl
 
@@ -49,12 +53,11 @@ modprobe overlay
 modprobe br_netfilter
 
 # Set up required sysctl params, these persist across reboots
-bash -c 'cat <<EOF >/etc/sysctl.d/k8s.conf
+cat <<EOF > /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
-EOF'
+EOF
 
 sysctl --system
 
 echo "Kubernetes installation on Ubuntu/Debian completed!"
-
